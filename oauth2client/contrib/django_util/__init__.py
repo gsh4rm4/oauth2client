fd6806d746a4b758f5d3343e7c35e20e1bedcb6f
gsh4rm4 <gsharma@gmail.com>
2017-11-30 17:12:44 -0800

    Django 2.0 Support Starting django 1.10 urlresolvers were deprecated. The functions were moved to django.urls. Django 2.0 removes the old functions from django.core.urlresolvers which breaks the oauth2client when using Django 2.0. This fix should support Django 2.0 and Django versions prior to 1.10.

--- oauth2client/contrib/django_util/__init__.py
+++ oauth2client/contrib/django_util/__init__.py
@@ -230,7 +230,10 @@ import importlib
 
 import django.conf
 from django.core import exceptions
-from django.core import urlresolvers
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
 from six.moves.urllib import parse
 
 from oauth2client import clientsecrets
@@ -408,7 +411,7 @@ def _redirect_with_params(url_name, *args, **kwargs):
     Returns:
         A properly formatted redirect string.
     """
-    url = urlresolvers.reverse(url_name, args=args)
+    url = reverse(url_name, args=args)
     params = parse.urlencode(kwargs, True)
     return "{0}?{1}".format(url, params)
 
